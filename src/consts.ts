8a2a21d24dc2be1a9b36a35ee0a0a55f01770e50
Rithvik Sopinti <rithvikpersonal0.0@gmail.com>
2025-02-25 09:40:46 +0530

    Add files via upload

---
+++ src/consts.ts
@@ -0,0 +1,23 @@
+import type { Links, Site } from "@/types";
+
+export const SITE: Site = {
+  TITLE: "RS",
+  DESCRIPTION: "rithvik's personal website",
+  AUTHOR: "Rithvik",
+  REPO_URL: "https://github.com/rithviksopinti/rithviksopinti.github.io",
+};
+
+export const LINKS: Links = [
+  {
+    TEXT: "Home",
+    HREF: "/",
+  },
+  {
+    TEXT: "About",
+    HREF: "/about",
+  },
+  {
+    TEXT: "Projects",
+    HREF: "/projects",
+  },
+];
